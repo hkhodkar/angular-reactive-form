16a3c32c2618681329ec689045941a0a8c634221
hatef khodkar <hatef.khodkar@hotmail.com>
2024-10-17 11:14:26 +0400

    angular reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+
+export interface IForm {
+  password: FormControl<string | null>;
+  confirmPassword: FormControl<string | null>;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+  }
   title = 'form';
+
+  form: FormGroup<IForm> = new FormGroup({
+    password: new FormControl<string>('', Validators.compose([Validators.required])),
+    confirmPassword: new FormControl<string>('', Validators.required)
+  },);
+
+  get passwordControl(): FormControl {
+    return this.form.controls.password;
+  }
+
+  get confirmPasswordControl(): FormControl {
+    return this.form.controls.confirmPassword;
+  }
+
 }
